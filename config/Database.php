27f432d6c0425215a68d3100b17bcaf4f5480081
Darcy Stacy <d_stacy@mail.fhsu.edu>
2022-03-21 18:26:34 -0400

    cleaned up db

--- config/Database.php
+++ config/Database.php
@@ -1,17 +1,5 @@
 <?php
     class Database {
-        /* connection for Heroku deployment
-        private $host = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        private $db_name = 'vkut027eqotv1gye';
-        private $username = 'ohq3wp4ybf8y1sqf';
-        private $conn;
-
-        local connection for testing pre-deployment
-        private $host = 'localhost';
-        private $db_name = 'quotesdb';
-        private $username = 'root';
-        private $conn;
-        */
 
         private $url;
         private $conn;
@@ -32,9 +20,8 @@
             $db_name = ltrim($dbparts['path'],'/');  
 
             try {
-               // $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password); //getenv('JAWSDB_PASS'));
-               $this->conn = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-               $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->conn = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 }
             catch(PDOException $e)
                 {
